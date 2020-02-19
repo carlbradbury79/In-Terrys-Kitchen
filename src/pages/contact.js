0224b08cbcd03b6ccea220676e9b2d7983f2873f
Carl Bradbury <carl@gnarlydog.co.uk>
2020-02-19 19:36:29 +0000

    Contact style

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,48 +1,120 @@
 import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
+import styled from "styled-components"
+
+const TextContainer = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  margin-top: 50px;
+
+  p {
+    font-size: 1.2rem;
+    line-height: 1.6;
+  }
+
+  @media (min-width: 700px) {
+    grid-template-columns: 1fr 1fr;
+    margin-top: 20px;
+  }
+`
+
+const Contact = styled.div`
+  box-sizing: border-box;
+  padding: 20px;
+  form {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 20px;
+  }
+
+  form label {
+    display: block;
+  }
+
+  form p {
+    margin: 10px;
+    display: block;
+  }
+
+  form .full {
+    grid-column: 1/3;
+  }
+
+  form button,
+  form input,
+  form textarea {
+    width: 90%;
+    border: 1px solid var(--terry-four);
+    padding: 1rem;
+  }
+
+  form button {
+    background: var(--terry-five);
+    border: 0;
+    text-transform: uppercase;
+    color: white;
+  }
+
+  form button:hover,
+  form button:focus {
+    background: var(--terry-four);
+    transition: background-color 2s ease-out;
+  }
+`
 
 const contact = () => (
   <Layout>
     <SEO title="Contact" />
-    <p>
-      I value your interest in my website and my cooking classes so if I’ve
-      missed something that you need to know before signing up or have comments
-      or suggestions please take a moment to fill in the contact form and send
-      it on.
-    </p>
-    <p>
-      I check this page often so do know that I’ll be happy to respond quickly.
-      If you’d prefer to receive a phone call please include your number (area
-      code+number) and a good time to call and I’ll call you back!
-    </p>
-
-    <form
-      name="contact"
-      method="post"
-      netlify-honeypot="bot-field"
-      data-netlify="true"
-    >
-      <input type="hidden" name="bot-field" />
-      <label>
-        Name
-        <input type="text" name="name" id="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" name="email" id="email" />
-      </label>
-      <label>
-        Subject
-        <input type="text" name="subject" id="subject" />
-      </label>
-      <label>
-        Message
-        <textarea name="message" id="message" rows="5" />
-      </label>
-      <button type="submit">Send</button>
-      <input type="reset" value="Clear" />
-    </form>
+    <TextContainer>
+      <div>
+        <p>
+          I value your interest in my website and my cooking classes so if I’ve
+          missed something that you need to know before signing up or have
+          comments or suggestions please take a moment to fill in the contact
+          form and send it on.
+        </p>
+        <p>
+          I check this page often so do know that I’ll be happy to respond
+          quickly. If you’d prefer to receive a phone call please include your
+          number (area code+number) and a good time to call and I’ll call you
+          back!
+        </p>
+      </div>
+      <Contact>
+        <form
+          name="contact"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
+          <input type="hidden" name="bot-field" />
+          <p>
+            <label>Name</label>
+            <input type="text" name="name" id="name" />
+          </p>
+          <p>
+            <label>Email</label>
+            <input type="email" name="email" id="email" />
+          </p>
+          <p>
+            <label>Subject</label>
+            <input type="text" name="subject" id="subject" />
+          </p>
+          <p>
+            <label className="full">Message</label>
+            <textarea name="message" id="message" rows="5" />
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+          {/* <input type="reset" value="Clear" /> */}
+        </form>
+      </Contact>
+    </TextContainer>
   </Layout>
 )
 
--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -1,52 +1,73 @@
 import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
+import styled from "styled-components"
+
+const TextContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+
+  p,
+  h1,
+  h2 {
+    padding: 0 20px 0 20px;
+  }
+
+  p {
+    font-size: 1.4rem;
+    line-height: 1.6;
+  }
+`
 
 const testimonials = () => (
   <Layout>
     <SEO title="Testimonials" />
-    <p>
-      “Taking a class “In Terry’s Kitchen” is so much fun! Her technical
-      expertise, super fresh ingredients and fun personality always make for a
-      great time. I especially love the beautiful home setting, gourmet kitchen,
-      pizza forno and stunning views. Her menus are inspired by extensive
-      international travel and associations with top chefs and many dishes have
-      become favorites in my house. If you have a chance to come to Terry’s
-      Kitchen, do it! Bring your friends and you will have an amazing
-      experience.” – Katie Price, Architect
-    </p>
-    <p>
-      “Terry’s cooking classes are a luscious treat! Her love of cooking and
-      teaching combine to create a warm, nurturing and fun atmosphere. Most of
-      the time, I’m not an adventurous cook, but in Terry’s kitchen, I’ve
-      ventured to make things I would have never considered like grilled heads
-      of radicchio studded with Italian sausage, baked cardoons in a creamy
-      parmesan sauce, and a fresh almond polenta cake drizzled with rosemary
-      scented poached peaches. They were ALL delicious, and I came home ready to
-      tackle these new recipes on my own. From Italian cuisine to French
-      desserts and more, I highly recommend classes In Terry’s Kitchen.” –
-      Louanne Klein, Training and Development Consultant
-    </p>
-    <p>
-      “For my birthday my husband gave the amazing gift of a cooking class with
-      Terry for me and seven of my friends. In advance of the class I worked
-      with Terry to create the perfect class for my interests and needs at the
-      time. I was about to host a French dinner for 20 in my home as a school
-      fundraiser, so I wanted some instruction on basic French cooking. But,
-      being a busy working mom, I was also interested in good everyday ideas for
-      feeding the family. Terry was very accommodating and fun to work with. She
-      was a wonder at coming up with an exceptional balanced menu, weaving in
-      practical everyday tips and recipes. From the moment we entered her
-      gorgeous kitchen and garden to the moment we left many hours later we were
-      well-fed, enlightened, and, most of all, enamored by Terry’s warm and
-      generous personality. Her hospitality is unsurpassed and her expertise in
-      finding and working with fresh ingredients inspiring. We prepared a myriad
-      of delights, from delicious pizzas loaded with fresh veggies in her
-      outdoor pizza oven to a fabulous beurre blanc sauce that I have made
-      several times since to many ooohs and aaahs. Terry’s classes are such a
-      find. I can’t wait to see what else is cooking In Terry’s Kitchen.” –
-      Selena Spain, Tech Writer
-    </p>
+    <TextContainer>
+      <p>
+        “Taking a class “In Terry’s Kitchen” is so much fun! Her technical
+        expertise, super fresh ingredients and fun personality always make for a
+        great time. I especially love the beautiful home setting, gourmet
+        kitchen, pizza forno and stunning views. Her menus are inspired by
+        extensive international travel and associations with top chefs and many
+        dishes have become favorites in my house. If you have a chance to come
+        to Terry’s Kitchen, do it! Bring your friends and you will have an
+        amazing experience.” – Katie Price, Architect
+      </p>
+      <p>
+        “Terry’s cooking classes are a luscious treat! Her love of cooking and
+        teaching combine to create a warm, nurturing and fun atmosphere. Most of
+        the time, I’m not an adventurous cook, but in Terry’s kitchen, I’ve
+        ventured to make things I would have never considered like grilled heads
+        of radicchio studded with Italian sausage, baked cardoons in a creamy
+        parmesan sauce, and a fresh almond polenta cake drizzled with rosemary
+        scented poached peaches. They were ALL delicious, and I came home ready
+        to tackle these new recipes on my own. From Italian cuisine to French
+        desserts and more, I highly recommend classes In Terry’s Kitchen.” –
+        Louanne Klein, Training and Development Consultant
+      </p>
+      <p>
+        “For my birthday my husband gave the amazing gift of a cooking class
+        with Terry for me and seven of my friends. In advance of the class I
+        worked with Terry to create the perfect class for my interests and needs
+        at the time. I was about to host a French dinner for 20 in my home as a
+        school fundraiser, so I wanted some instruction on basic French cooking.
+        But, being a busy working mom, I was also interested in good everyday
+        ideas for feeding the family. Terry was very accommodating and fun to
+        work with. She was a wonder at coming up with an exceptional balanced
+        menu, weaving in practical everyday tips and recipes.{" "}
+      </p>
+      <p>
+        From the moment we entered her gorgeous kitchen and garden to the moment
+        we left many hours later we were well-fed, enlightened, and, most of
+        all, enamored by Terry’s warm and generous personality. Her hospitality
+        is unsurpassed and her expertise in finding and working with fresh
+        ingredients inspiring. We prepared a myriad of delights, from delicious
+        pizzas loaded with fresh veggies in her outdoor pizza oven to a fabulous
+        beurre blanc sauce that I have made several times since to many ooohs
+        and aaahs. Terry’s classes are such a find. I can’t wait to see what
+        else is cooking In Terry’s Kitchen.” – Selena Spain, Tech Writer
+      </p>
+    </TextContainer>
   </Layout>
 )
 
